d7263c8137c62663320c23b227b1816a26a19849
Karoline <boothwhack@users.noreply.github.com>
2024-11-21 21:16:51 +0100

    Add Windows build script

---
+++ BuildGraphviz.ps1
@@ -0,0 +1,26 @@
+$SourceDir = "$PSScriptRoot\graphviz"
+$BuildDir = "$SourceDir\build"
+$PrefixDir = "$SourceDir\install"
+$OutputDir = "$PSScriptRoot\dist"
+$OutputSrcDir = "$OutputDir\src"
+$OutputLibDir = "$OutputDir\lib"
+
+# Apply patch to generate CSharp code in GraphVizard.Interop namespace
+git apply --directory "$SourceDir" graphviz-namespace.patch
+
+# Configure Graphviz build
+cmake -S "$SourceDir" -B "$BuildDir" `
+    -DENABLE_SWIG=true `
+    -DENABLE_SHARP=true
+
+# Build Graphviz library
+cmake --build "$BuildDir"
+
+# Install built DLL and CSharp files into prefix
+cmake --install "$BuildDir" --prefix "$PrefixDir"
+
+# Copy generated CSharp code and glue library into project directory
+Write-Output "Copying generated files into: $OutputDir"
+md "$OutputDir" -ea 0 | Out-Null
+copy "$PrefixDir\lib\graphviz\sharp\*.cs" "$OutputSrcDir"
+copy "$PrefixDir\lib\graphviz\sharp\*.dll" "$OutputLibDir"
