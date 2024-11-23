485ac8c20b18c4c9a7b35b2f0f0ec3c2d6f52e0c
Karoline <boothwhack@users.noreply.github.com>
2024-11-23 22:27:40 +0100

    Add Extensions

---
+++ GraphVizard.Interop/src/Extensions.cs
@@ -0,0 +1,30 @@
+using System.Runtime.InteropServices;
+
+namespace GraphVizard.Interop;
+
+public static class Extensions
+{
+    public static IntPtr AGDATA(SWIGTYPE_p_Agraph_t n)
+    {
+        /*
+         * Imitates C macro: #define AGDATA(obj) (((Agobj_t *)(obj))->data)
+         * Assumes 'n' is a pointer to struct Agobj_s
+         * offsetof(Agobj_s, Agobj_s::data) == 16
+         */
+
+        return SWIGTYPE_p_Agraph_t.getCPtr(n).Handle + 16;
+    }
+
+    public static Position ND_coord(SWIGTYPE_p_Agraph_t n)
+    {
+        /*
+         * Imitates C macro: #define ND_coord(n) (((Agnodeinfo_t*)AGDATA(n))->coord)
+         * Assumes 'n' is pointer to struct Agnodeinfo_t
+         * offsetof(Agnodeinfo_t, Agnodeinfo_t::coord) == 32
+         */
+
+        var data = Marshal.ReadIntPtr(AGDATA(n));
+        var ptr = data + 32;
+        return Marshal.PtrToStructure<Position>(ptr);
+    }
+}
---
+++ GraphVizard.Interop/src/Position.cs
@@ -0,0 +1,7 @@
+namespace GraphVizard.Interop;
+
+public struct Position
+{
+    public double X;
+    public double Y;
+}
