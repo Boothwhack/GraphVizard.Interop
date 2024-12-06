5ef570ea5b8e66b28a31dfa10d6335477b5442dc
Karoline <19309513+Boothwhack@users.noreply.github.com>
2024-12-06 13:15:13 +0000

    Build portable

--- build-graphviz.sh
+++ build-graphviz.sh
@@ -16,8 +16,22 @@ popd
 
 # Configure Graphviz build
 cmake -S "$source_dir" -B "$build_dir" \
-  -DENABLE_SWIG=true \
-  -DENABLE_SHARP=true
+  -DENABLE_SWIG=ON \
+  -DENABLE_SHARP=ON \
+  -DWITH_SMYRNA=OFF \
+  -DWITH_GVEDIT=OFF \
+  -DWITH_SMYRNA=OFF \
+  -DENABLE_TCL=OFF \
+  -DENABLE_D=OFF \
+  -DENABLE_GO=OFF \
+  -DENABLE_GUILE=OFF \
+  -DENABLE_JAVA=OFF \
+  -DENABLE_JAVASCRIPT=OFF \
+  -DENABLE_LUA=OFF \
+  -DENABLE_PERL=OFF \
+  -DENABLE_PHP=OFF \
+  -DENABLE_PYTHON=OFF \
+  '-DCMAKE_INSTALL_RPATH=$ORIGIN'
 
 # Build Graphviz library
 cmake --build "$build_dir"
@@ -27,6 +41,18 @@ cmake --install "$build_dir" --prefix "$prefix_dir"
 
 # Copy generated CSharp code and glue library into project directory
 echo "Copying generated files into: $output_dir"
-mkdir -p "$output_src_dir" "$output_lib_dir"
+mkdir -p "$output_src_dir" "$output_lib_dir/graphviz"
+
+# Copy generated CSharp code to gen source directory
 cp "$prefix_dir/lib/graphviz/sharp/"*.cs "$output_src_dir"
+
+shopt -s extglob
+
+# Copy built graphviz binaries into lib directory
+# Only include version libs, eg. libgvc.so.6
+cp "$prefix_dir/lib/"*.so.+([0-9]) "$output_lib_dir"
+cp "$prefix_dir/lib/graphviz/"*.so.+([0-9]) "$output_lib_dir/graphviz"
+cp "$prefix_dir/lib/graphviz/"config* "$output_lib_dir/graphviz"
+
+# Copy glue library up into root lib directory
 cp "$prefix_dir/lib/graphviz/sharp/"*.so "$output_lib_dir"
